e7081fc9035dc0640d30b4a4699045f693de7f21
jiwon <jiwoniy@gmail.com>
2020-04-28 17:45:12 +0900

    wip

--- types/interface.go
+++ types/interface.go
@@ -1,5 +1,16 @@
 package types
 
+type Parser interface {
+	Read(filename string) ([]byte, error)
+	Print(v interface{})
+	Parse(data []byte, v interface{}) error
+	// Result()
+	// GetTx(hash string) (*TxInfo, error)
+	// GetTxsFromHeight(height uint64) ([]string, error)
+	// GetLatestHeight() (uint64, error)
+	// GetNetwork() Network
+}
+
 type Crawler interface {
 	// GetTx(hash string) (*TxInfo, error)
 	// GetTxsFromHeight(height uint64) ([]string, error)
